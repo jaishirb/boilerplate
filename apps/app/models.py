07c6910ccea252b20d4d097e4caffb6338bd1299
Jaisir Bayuelo <jaisirenterprise@gmail.com>
2019-10-28 10:45:25 -0500

    ll

--- apps/app/models.py
+++ apps/app/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.contrib.gis.db import models
+from django.db import models
 from . import constants
 
 
--- apps/main/models.py
+++ apps/main/models.py
@@ -1,1 +1,1 @@
-from django.contrib.gis.db import models
+from django.db import models
